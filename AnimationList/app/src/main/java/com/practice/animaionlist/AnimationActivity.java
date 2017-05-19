88bd6012c39a71ea8d9b1110da58e048d3ac709e
AmitaAndroid <0amita0@gmail.com>
2017-05-20 01:32:25 +0530

    UI ready

---
+++ AnimationList/app/src/main/java/com/practice/animaionlist/AnimationActivity.java
@@ -0,0 +1,18 @@
+package com.practice.animaionlist;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class AnimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation);
+
+    }
+}
--- AnimationList/app/src/main/java/com/practice/animaionlist/MainActivity.java
+++ AnimationList/app/src/main/java/com/practice/animaionlist/MainActivity.java
@@ -2,12 +2,25 @@ package com.practice.animaionlist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    String[] animationType={"anim1"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intiView();
+    }
+
+    private void intiView() {
+        listView=(ListView)findViewById(R.id.list_animation);
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1 , animationType);
+        listView.setAdapter(adapter);
     }
 }
